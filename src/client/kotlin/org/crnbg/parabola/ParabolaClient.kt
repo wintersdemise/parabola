cbe0c0a0e72d3fdef782bece97eae1e797b0c1a2
Winter's Demise <138141040+wintersdemise@users.noreply.github.com>
2025-12-24 18:29:53 +0100

    СМРТ

---
+++ src/client/kotlin/org/crnbg/parabola/ParabolaClient.kt
@@ -0,0 +1,9 @@
+package org.crnbg.parabola
+
+import net.fabricmc.api.ClientModInitializer
+
+object ParabolaClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/kotlin/org/crnbg/parabola/ParabolaDataGenerator.kt
@@ -0,0 +1,9 @@
+package org.crnbg.parabola
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object ParabolaDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
